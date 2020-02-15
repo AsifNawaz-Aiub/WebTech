24ba822a74b09d60453d76e39c3728fb5bd35d1c
AsifNawaz-Aiub <60336504+AsifNawaz-Aiub@users.noreply.github.com>
2020-02-16 01:27:47 +0600

    task

---
+++ show.php
@@ -0,0 +1,15 @@
+<html>
+<body>
+
+
+Your name is : <?php echo $_POST["name"]; ?> <br>
+Your email is : <?php echo $_POST["email"]; ?> <br>
+Your birthdate is : <?php echo $_POST["birthdate"];?> <br>
+Your gender is : <?php echo $_POST["gender"];?> <br>
+Your degree is : <?php echo $_POST["degree"];?> <br>
+Your blood group is : <?php echo $_POST["group"];?> <br>
+
+
+
+</body>
+</html>
